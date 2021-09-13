6cdd8cabfef34821b29cdde4af8ebecd504a450a
Axel Rasse <rasseax@gmail.com>
2021-09-13 15:43:39 +0400

    feat: CUD on page.content is now available

--- Notion/driver/driver.py
+++ Notion/driver/driver.py
@@ -1,23 +1,56 @@
-from utils.request import RequestPageProperties, RequestPage
-from utils.page import PageObject
+from utils.request import RequestPage, RequestBlock
+from utils.page import PageProperties, PageContent
+from typing import Dict
+from utils.connect import Notion
+
+VERSION = "2021-08-16"
 
 
-class Page(PageObject):
-    def __init__(self, page_url, token_api) -> None:
-        self.token_api = token_api
+# class Notion:
+#     headers = {}
+
+#     @classmethod
+#     def connect(cls, token: str) -> None:
+#         cls.headers = {
+#             "Authorization": f"Bearer {token}",
+#             "Notion-Version": f"{VERSION}",
+#             "Content-Type": "application/json",
+#         }
+
+
+class Page:
+    def __init__(self, page_url) -> None:
         self.url = page_url
         self.id = page_url.split("-")[-1]
 
-        self.raw = RequestPage(self.id, self.token_api)
-        super().__init__(**self.raw.retreive())
+        self._properties = RequestPage(self.id, Notion.headers).retreive()
+        self.created_time = self._properties["created_time"]
+        self.last_edited_time = self._properties["last_edited_time"]
+        self.archived = self._properties["archived"]
+        self.icon = self._properties["icon"]
+        self.cover = self._properties["cover"]
+        self.properties = PageProperties(self._properties, Notion.headers)
+        self.parent = self._properties["parent"]
+
+        self._content = RequestBlock(self.id, Notion.headers).retreive_children()
+        self.content = PageContent(self._content, self.id, Notion.headers)
 
     def update(self):
-        RequestPageProperties(self.id, self.token_api).update(self.raw.properties)
-        for block in self.raw.content:
-            RequestPageProperties(block["id"], self.token_api).update(block)
+        self.properties.update()
+        # for block in self.raw.content:
+        #     RequestPageProperties(block["id"], self.token_api).update(block)
 
     def refresh(self):
+        """Retreive Page properties & content."""
         return self.__init__(self.url)
+
+    def append(self, block_object: Dict) -> None:
+        """Creates and appends new children blocks to the parent block_id specified."""
+        RequestBlock(self.id, Notion.headers).append_children(block_object)
+
+    def delete(self):
+        """Archiving workspace level pages via API not supported."""
+        RequestBlock(self.id, Notion.headers).delete()
 
     # def duplicate(self):
     #     template = TemplateObject(self, self.content.raw, self.token_api).create()
@@ -27,10 +60,10 @@ class Page(PageObject):
 
 if __name__ == "__main__":
 
-    TOKEN_API = "secret_R1CrUGn8bx9itbJW0Fc9Cc0R9Lmhbnz2ayqEe0GhRPq"
-    PAGE_URL = "https://www.notion.so/Bitcoin-aaca5f6afecc4bbfa29c8d2e17901e18"
+    from utils.block import *
 
-    page = Page(PAGE_URL, TOKEN_API)
-    # page.properties["Email"] = "axelito@gmail.com"
-    # page.properties["Completion Time"] = 4
-    # page.update()
+    TOKEN_API = "secret_R1CrUGn8bx9itbJW0Fc9Cc0R9Lmhbnz2ayqEe0GhRPq"
+    PAGE_URL = "https://www.notion.so/Axel-2ccdafe28955478b8c9d70bda0044c86"
+
+    Notion.connect(TOKEN_API)
+    page = Page(PAGE_URL)
--- Notion/driver/utils/page.py
+++ Notion/driver/utils/page.py
@@ -1,52 +1,64 @@
 from typing import Dict
 import pandas as pd
 from .property import Property
-from .block import BlockObject
+from .block import Block, extract_block, insert_block
+from .request import RequestPage, RequestBlock
 
 
 class PageProperties:
-    def __init__(self, properties: Dict):
+    def __init__(self, properties: Dict, headers: Dict):
+        self.headers = headers
+
         self.raw = properties
+        self._properties = properties["properties"]
+        self.parent_id = properties["id"]
 
     def __getitem__(self, key):
-        return Property(self.raw[key]).extract()
+        return Property(self._properties[key]).extract()
 
     def __setitem__(self, key, value):
-        return Property(self.raw[key]).insert(value)
-
-    def get(self) -> pd.Series:
-        data = {key: self[key] for key in self.raw.keys()}
-        return pd.Series(data)
+        Property(self._properties[key]).insert(value)
 
     def __repr__(self) -> str:
         return f"{self.get()}"
 
+    def get(self) -> pd.Series:
+        data = {key: self[key] for key in self._properties.keys()}
+        return pd.Series(data)
+
+    def update(self) -> None:
+        RequestPage(self.parent_id, self.headers).update(self.raw)
+
 
 class PageContent:
-    def __init__(self, blocks) -> None:
-        self.raw = [block for block in blocks]
+    def __init__(self, blocks, page_id, headers) -> None:
+        self.raw = blocks
+        self.page_id = page_id
+        self.headers = headers
 
     def __getitem__(self, index):
-        return BlockObject(self.raw[index]).extract()
+        return extract_block(self.raw[index])
 
     def __setitem__(self, index, value):
-        return BlockObject(self.raw[index]).insert(value)
-
-    def get(self) -> pd.DataFrame:
-        result = []
-        for block in self.raw:
-            block = BlockObject(block)
-            result.append(
-                {
-                    "type": block.type,
-                    "content": block.extract(),
-                    "id": block.id,
-                }
-            )
-        return pd.DataFrame(result)
+        insert_block(self.raw[index], value)
 
     def __repr__(self) -> str:
         return f"{self.get()}"
 
     def _repr_html_(self):
         return self.get().to_html()
+
+    def get(self) -> pd.DataFrame:
+        result = []
+        for block in self.raw:
+            result.append(
+                {
+                    "type": block.get("type"),
+                    "content": extract_block(block),
+                    "id": block.get("id"),
+                }
+            )
+        return pd.DataFrame(result)
+
+    def append(self, block: Block) -> None:
+        RequestBlock(self.page_id, self.headers).append_children(block)
